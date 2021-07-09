9f0e7cf5bcb7cc7697b1ec6c01313f5b439566c6
Consecratio <jcasique@gmail.com>
2021-07-09 16:52:58 -0700

    lab finished

--- lab.js
+++ lab.js
@@ -1,3 +1,4 @@
+var _a;
 console.clear();
 /* TS Algorithms Lab */
 /* --- Problem #1 - variable types --- */
@@ -7,8 +8,60 @@ console.clear();
 // console.log(realNumber) => 32
 var numberString = '42';
 var realNumber = 7;
-// Todo - Create union type here
+_a = [String(realNumber), Number(numberString)], numberString = _a[0], realNumber = _a[1];
+// console.log(numberString)
+// console.log(realNumber)
+// todo: swap the values in the varaibles and log the results
+/* --- Problem #2 - function and mixed array type --- */
+// 1. create an array type that is a mix of strings of numbers and actual numbers
+// ex: [10, '34', 3, '17']
+var mixedArray = [10, '34', 3, '17'];
+// console.log(mixedArray)
+// 2. write a function that accepts an array as a parameter, loops over it and converts all the 
+// strings to numbers. the function should return a new array of only numbers
+// ex: stringToNum([10, '34', 3, '17']) => [10, 34, 3, 17]
+function stringToNum(arr) {
+    var newArr = arr.map(function (item) { return Number(item); });
+    return newArr;
+}
+var morphedArr = stringToNum(mixedArray);
+// 2. Create some cities with lat/lon data and pass them into printCityData;
+// uncomment the function to test your solution
+var seattle = ['Seattle', 10, 20];
+// Todo - create the type definition, and some cities
+// 🚨 uncomment this function to test your solution 🚨
+function printCityData(city) {
+    console.log(city[0] + " is at Lat: " + city[1] + ", Long: " + city[2]);
+}
 // Todo - write area function here
-/* --- Problem #5 - Classes --- */
-// Bonus - Implement a Stack with TypeScript
-// https://initjs.org/data-structure-stack-in-javascript-714f45dbf889
+function area(shape) {
+    switch (shape.kind) {
+        case 'square':
+            return Math.pow(shape.width, 2);
+            break;
+        case 'rectangle':
+            return shape.width * shape.height;
+            break;
+        case 'circle':
+            return Math.PI * Math.pow(shape.radius, 2);
+            break;
+        default:
+            return 0;
+    }
+}
+var sqr = {
+    kind: 'square',
+    width: 10
+};
+var rect = {
+    kind: 'rectangle',
+    width: 10,
+    height: 15
+};
+var circ = {
+    kind: 'circle',
+    radius: 10
+};
+console.log(area(sqr));
+console.log(area(rect));
+console.log(area(circ));
--- lab.ts
+++ lab.ts
@@ -7,17 +7,34 @@ console.clear()
 // console.log(numberString) => '7'
 // console.log(realNumber) => 32
 
-let numberString: string = '42'
-let realNumber: number = 7
+let numberString: string = '42';
+let realNumber: number = 7;
+
+[numberString, realNumber] = [String(realNumber), Number(numberString)]
+
+// console.log(numberString)
+// console.log(realNumber)
 
 // todo: swap the values in the varaibles and log the results
 
 /* --- Problem #2 - function and mixed array type --- */
 // 1. create an array type that is a mix of strings of numbers and actual numbers
 // ex: [10, '34', 3, '17']
+
+let mixedArray: Array< string | number > = [10, '34', 3, '17']
+// console.log(mixedArray)
+
 // 2. write a function that accepts an array as a parameter, loops over it and converts all the 
 // strings to numbers. the function should return a new array of only numbers
 // ex: stringToNum([10, '34', 3, '17']) => [10, 34, 3, 17]
+
+function stringToNum(arr: Array< string | number >): Array<number> {
+    let newArr = arr.map(item => Number(item))
+    return newArr
+}
+
+let morphedArr = stringToNum(mixedArray)
+// console.log(morphedArr)
 
 // Todo write the mixed array of strings and numbers
 
@@ -25,15 +42,20 @@ let realNumber: number = 7
 
 /* --- Problem #3 - Tuple Type --- */
 // 1. Create a tuple type named 'cityCoordinate' that has three elements: city name, latitute, and longitude
+
+type cityCoordinate = [string, number, number]
+
 // 2. Create some cities with lat/lon data and pass them into printCityData;
 // uncomment the function to test your solution
+
+let seattle: cityCoordinate = ['Seattle', 10, 20]
 
 // Todo - create the type definition, and some cities
 
 // 🚨 uncomment this function to test your solution 🚨
-// function printCityData(city: cityCoordinate): void {
-//     console.log(`${city[0]} is at Lat: ${city[1]}, Long: ${city[2]}`);
-// }
+function printCityData(city: cityCoordinate): void {
+    console.log(`${city[0]} is at Lat: ${city[1]}, Long: ${city[2]}`);
+}
 
 // printCityData(seattle);
 
@@ -61,5 +83,41 @@ interface Circle {
 }
 
 // Todo - Create union type here
+type union = (Square | Rectangle | Circle)
 
 // Todo - write area function here
+function area(shape: union): number {
+    switch(shape.kind) {
+        case 'square':
+            return Math.pow(shape.width, 2)
+            break
+        case 'rectangle':
+            return shape.width * shape.height
+            break
+        case 'circle':
+            return Math.PI * Math.pow(shape.radius, 2)
+            break
+        default:
+            return 0
+    }
+}
+
+let sqr: Square = {
+    kind: 'square',
+    width: 10
+}
+
+let rect: Rectangle = {
+    kind: 'rectangle',
+    width: 10,
+    height: 15
+}
+
+let circ: Circle = {
+    kind: 'circle',
+    radius: 10
+}
+
+console.log(area(sqr))
+console.log(area(rect))
+console.log(area(circ))
